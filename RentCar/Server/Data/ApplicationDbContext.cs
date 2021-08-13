d53f6916cb1fb2694528970c7696abeeb0c5d009
Carlos Maia <carlos.marques89@gmail.com>
2021-08-13 16:44:00 -0400

    Added more tables and migration

--- RentCar/Server/Data/ApplicationDbContext.cs
+++ RentCar/Server/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using RentCar.Server.Models;
+using RentCar.Shared.Domain;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,5 +18,11 @@ namespace RentCar.Server.Data
             IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
         {
         }
+        public DbSet<Vehicle> Vehicles { get; set; }
+        public DbSet<Colour> Colours { get; set; }
+        public DbSet<Make> Makes { get; set; }
+        public DbSet<Model> Models { get; set; }
+        public DbSet<Customer> Customer { get; set; }
+        public DbSet<Booking> Booking { get; set; }
     }
 }
